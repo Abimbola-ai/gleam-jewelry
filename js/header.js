ddea69ed5e0c401993a29047a30440084abcaae6
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-07-14 04:30:45 -0400

    fixed curency option

--- js/header.js
+++ js/header.js
@@ -3,6 +3,7 @@
 window.addEventListener('scroll', function () {
   let header = document.querySelector('header');
   let headerIcon = document.querySelectorAll('.header-icon');
+  let currency = document.querySelector('#currency');
   let scrollPosition = window.scrollY;
 
   if (scrollPosition > 0) {
@@ -10,10 +11,12 @@ window.addEventListener('scroll', function () {
     headerIcon.forEach(function (icon) {
       icon.style.color = 'black';
     });
+    currency.style.color = 'black';
   } else {
     header.classList.remove('scrolled');
     headerIcon.forEach(function (icon) {
       icon.style.color = 'white';
     });
+    currency.style.color = 'white';
   }
 });
