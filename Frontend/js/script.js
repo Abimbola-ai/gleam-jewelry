045a158e63cd852113cb2cc1ccad99c984c19a5d
Abimbola Ojikutu <ojikutu.abimbola@gmail.com>
2023-07-25 12:13:49 -0400

    updated files and product file code pushed

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,15 +1,19 @@
-// Password confirmation script
+// // Password confirmation script
 
-let password = document.getElementById("password");
-let confirm_password = document.getElementById("confirm_password");
+// let password = document.getElementById("password");
+// let confirm_password = document.getElementById("confirm_password");
 
-function validatePassword(){
-  if(password.value != confirm_password.value) {
-    confirm_password.setCustomValidity("Passwords Don't Match");
-  } else {
-    confirm_password.setCustomValidity('');
-  }
-}
+// function validatePassword(){
+//   if(password.value != confirm_password.value) {
+//     confirm_password.setCustomValidity("Passwords Don't Match");
+//   } else {
+//     confirm_password.setCustomValidity('');
+//   }
+// }
 
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
+// password.onchange = validatePassword;
+// confirm_password.onkeyup = validatePassword;
+
+
+
+
