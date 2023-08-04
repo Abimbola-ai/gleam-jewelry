33618ad05832cf2c7269eb5c5ad481e5579c81ae
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-08-04 08:39:56 -0400

    created function to generate order number

--- frontend/js/shipping.js
+++ frontend/js/shipping.js
@@ -1,1 +1,10 @@
-alert('it works');
+// generate order number as a random string.
+const generatOrderNumber = function (length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let result = '';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
