675eb874f743c515f7eb47e951b8ed057c68a75d
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-08-04 19:16:23 -0400

    adding grand total

--- frontend/js/shipping.js
+++ frontend/js/shipping.js
@@ -12,14 +12,31 @@ const generatOrderNumber = function (length) {
 // Update the order number element with the generated order number
 document.getElementById('order-number').textContent = generatOrderNumber(11);
 
-// Function to update the order summary
-function updateOrderSummary(subtotal, tax) {
-  const shippingFee = 20.0;
-  const grandTotal = subtotal + shippingFee + tax;
+// Use JavaScript to extract the subtotal value from the URL
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const subtotal = urlParams.get('subtotal');
+const tax = urlParams.get('tax');
 
-  document.getElementById('subtotal').textContent = `$${subtotal.toFixed(2)}`;
-  document.getElementById('tax').textContent = `$${tax.toFixed(2)}`;
-  document.getElementById('grand-total').textContent = `$${grandTotal.toFixed(
-    2
-  )}`;
+// Display the subtotal and tax
+const subtotalElement = document.getElementById('subtotal');
+const taxElement = document.getElementById('tax');
+
+if (subtotalElement && taxElement) {
+  subtotalElement.textContent = `$${subtotal}`;
+  taxElement.textContent = `$${tax}`;
+  console.log(subtotal);
+  console.log(tax);
+}
+
+// Calculate the grand total
+const shippingFee = 20.0;
+const grandTotal = parseFloat(subtotal) + shippingFee + parseFloat(tax);
+
+// Display the grand total
+const grandTotalElement = document.getElementById('grand-total');
+
+if (grandTotalElement) {
+  grandTotalElement.textContent = `$${grandTotal.toFixed(2)}`;
+  console.log(grandTotal);
 }
