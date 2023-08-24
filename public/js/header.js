643ba89b3ea0ec137bcad8448fec4c5149f5f3f5
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-08-24 13:50:11 -0400

    fixing changes from review meeting

--- public/js/header.js
+++ public/js/header.js
@@ -1,10 +1,10 @@
 let user = {};
-document.addEventListener('DOMContentLoaded', () => {
-  const welcomeUser = document.getElementById('welcome_user');
-  const signinLink = document.getElementById('signin');
-  const signupLink = document.getElementById('signup');
-  const logoutLink = document.getElementById('logout');
-  const headerIcons = document.getElementsByClassName('header-icon');
+document.addEventListener("DOMContentLoaded", () => {
+  const welcomeUser = document.getElementById("welcome_user");
+  const signinLink = document.getElementById("signin");
+  const signupLink = document.getElementById("signup");
+  const logoutLink = document.getElementById("logout");
+  const headerIcons = document.getElementsByClassName("header-icon");
 
   // Retrieve the user data sent by the server
   // const userData = JSON.stringify(user);
@@ -14,13 +14,13 @@ document.addEventListener('DOMContentLoaded', () => {
   // Function to fetch user data from the API
   async function fetchUserData() {
     try {
-      const response = await fetch('/api/user'); // Call the API endpoint
+      const response = await fetch("/api/user"); // Call the API endpoint
       // console.log(response);
       const userData = await response.json(); // Parse the JSON response
       user = userData;
       return userData;
     } catch (error) {
-      console.error('Error fetching user data:', error);
+      console.error("Error fetching user data:", error);
       return {}; // Return an empty object if there's an error
     }
   }
@@ -31,40 +31,40 @@ document.addEventListener('DOMContentLoaded', () => {
     if (userData.first_name) {
       // User is logged in, display user name and hide Sign In and Create an account
       welcomeUser.textContent = `Hello, ${userData.first_name}`;
-      headerIcons.style.display = 'none';
-      logoutLink.style.display = 'block'; // Show Log out link - this will be in the dropdown in the hamburger menu.
+      headerIcons.style.display = "none";
+      logoutLink.style.display = "block"; // Show Log out link - this will be in the dropdown in the hamburger menu.
     } else {
       // User is not logged in, hide Log out link and display Sign In and Create an account
-      welcomeUser.style.display = 'none';
-      signinLink.style.display = 'block'; // Show Sign In link
-      signupLink.style.display = 'block'; // Show Create an account link
-      logoutLink.style.display = 'none';
+      welcomeUser.style.display = "none";
+      signinLink.style.display = "block"; // Show Sign In link
+      signupLink.style.display = "block"; // Show Create an account link
+      logoutLink.style.display = "none";
     }
   });
 });
 
 // code for changing header when the page scrolls
 
-window.addEventListener('scroll', function () {
-  let header = document.querySelector('header');
-  let headerIcon = document.querySelectorAll('.header-icon');
-  let currency = document.querySelector('#currency');
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  let headerIcon = document.querySelectorAll(".header-icon");
+  let currency = document.querySelector("#currency");
   let scrollPosition = window.scrollY;
-  let greeting = document.getElementById('#welcome_user');
+  let greeting = document.getElementById("welcome_user");
 
   if (scrollPosition > 0) {
-    header.classList.add('scrolled');
+    header.classList.add("scrolled");
     headerIcon.forEach(function (icon) {
-      icon.style.color = 'black';
+      icon.style.color = "black";
     });
-    currency.style.color = 'black';
-    // greeting.style.color = 'black';
+    currency.style.color = "black";
+    greeting.style.color = "black";
   } else {
-    header.classList.remove('scrolled');
+    header.classList.remove("scrolled");
     headerIcon.forEach(function (icon) {
-      icon.style.color = 'white';
+      icon.style.color = "white";
     });
-    currency.style.color = 'white';
-    // greeting.style.color = 'white';
+    currency.style.color = "white";
+    greeting.style.color = "white";
   }
 });
